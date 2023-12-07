8c405422def99081f2418afab24ba9c547fc7c45
Ken Leidal <kkleidal@gmail.com>
2023-12-07 18:42:45 -0500

    day 6

---
+++ day6/part1.py
@@ -0,0 +1,20 @@
+import sys
+import re
+from functools import reduce
+
+with open(sys.argv[1], "r") as f:
+    for line in f:
+        splits = re.split(r"\s+", line.strip())
+        parts = [int(part) for part in splits[1:]]
+        if splits[0] == "Time:":
+            times = parts
+        else:
+            assert splits[0] == "Distance:"
+            distances = parts
+
+prod = lambda x: reduce(lambda x, y: x * y, x, 1)
+
+print(prod(
+    sum(1 for hold in range(total_time) if (total_time - hold) * hold > best_distance)
+    for total_time, best_distance in zip(times, distances)
+))
